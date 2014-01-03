9334cbcb54812558fcd92eaea947a83a0c8f25a2
Samuli Seppänen <samuli@openvpn.net>
2014-01-03 14:26:54 +0000

    Added missing class documentation
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,17 @@
 #
 # == Parameters
 #
+# [*listen_addresses*]
+#   A space-separated list of IP address to listen on (see "BindAddress" in 
+#   apt-cacher-ng documentation). For example '0.0.0.0', 'server.domain.com' or 
+#   'localhost'. Defaults to 'localhost'
+# [*port*]
+#   The TCP port to listen on. Defaults to 3142.
+# [*allow_address_ipv4*]
+#   IPv4 address or address range to allow connections from. Defaults to 
+#   '127.0.0.1'.
+# [*allow_address_ipv6*]
+#   IPv4 address or address range to allow connections from. Defaults to '::1'.
 # [*monitor_email*]
 #   Email address where local service monitoring software sends it's reports to. 
 #   Defaults to global variable $::servermonitor.
